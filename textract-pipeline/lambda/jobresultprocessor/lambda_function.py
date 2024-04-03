126b0584cbe8e269265f80e6f09b81a00f0c0888
AaronJFoster <aaron.foster@whichrate.co.uk>
2024-04-03 14:46:01 +0100

    Updates to get this to deploy to AWS
    Updates to get this to deploy to AWS - further updates to Python version required.

--- textract-pipeline/lambda/jobresultprocessor/lambda_function.py
+++ textract-pipeline/lambda/jobresultprocessor/lambda_function.py
@@ -69,6 +69,9 @@ def processRequest(request):
         detectForms = True
         detectTables = True
 
+    dynamodb = AwsHelper().getResource('dynamodb')
+    ddb = dynamodb.Table(outputTable)
+
     opg = OutputGenerator(jobTag, pages, bucketName, objectName, detectForms, detectTables, ddb)
     opg.run()
 
